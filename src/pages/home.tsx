b33a744b14370abf756cba32f09b32edc700d247
Jiye <hyeonjiyeah@gmail.com>
2022-11-10 06:20:39 +0900

    feat: home 컴포넌트 생성

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import type { PageProps } from "gatsby";
+import { graphql } from "gatsby";
+import styled from "@emotion/styled";
+
+const HomePage = function () {
+  return <>HomePage</>;
+};
+
+export default HomePage;
