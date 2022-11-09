7993903156a2ad4b4bb2a2d24b881c35ddb86172
Jiye <hyeonjiyeah@gmail.com>
2022-11-10 06:19:31 +0900

    feat: Header 컴포넌트 생성

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+import styled from "@emotion/styled";
+import { Link, graphql, PageProps } from "gatsby";
+
+const Header: FunctionComponent = function () {
+  return (
+    <header>
+      <h1>이름을 아직 못정한 블로그</h1>
+    </header>
+  );
+};
+
+export default Header;
