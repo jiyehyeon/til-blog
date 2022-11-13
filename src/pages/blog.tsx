208b2644d252cf9d775c9133d8bb673a271e8712
Jiye <hyeonjiyeah@gmail.com>
2022-11-13 17:28:41 +0900

    edit: 빌드오류 수정

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -64,7 +64,7 @@ export const query = graphql`
             }
           }
           fileAbsolutePath
-          excerpt(format: MARKDOWN)
+          excerpt
           frontmatter {
             date(formatString: "YYYY-MM-DD")
             title
