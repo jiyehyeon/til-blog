a101e0c7367391d86657174eafd2258501316e46
Jiye <hyeonjiyeah@gmail.com>
2022-11-14 16:48:48 +0900

    edit: 폰트 변경

--- src/components/common/GlobalStyle.tsx
+++ src/components/common/GlobalStyle.tsx
@@ -3,18 +3,40 @@ import { Global, css } from "@emotion/react";
 
 const defaultStyle = css`
   @font-face {
-    font-family: "HallymGothic-Regular";
-    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2204@1.0/HallymGothic-Regular.woff2")
-      format("woff2");
-    font-weight: 400;
+    font-family: "LINESeedKR";
+    src: url("../../assets/fonts/EOT/LINESeedKR-Th.eot")
+        format("embedded-opentype"),
+      url("../../assets/fonts/WOFF/LINESeedKR-Th.woff") format("woff"),
+      url("../../assets/fonts/WOFF2/LINESeedKR-Th.woff2") format("woff2");
     font-style: normal;
+    font-weight: 100;
+  }
+
+  @font-face {
+    font-family: "LINESeedKR";
+    src: url("../../assets/fonts/EOT/LINESeedKR-Rg.eot")
+        format("embedded-opentype"),
+      url("../../assets/fonts/WOFF/LINESeedKR-Rg.woff") format("woff"),
+      url("../../assets/fonts/WOFF2/LINESeedKR-Rg.woff2") format("woff2");
+    font-style: normal;
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "LINESeedKR";
+    src: url("../../assets/fonts/EOT/LINESeedKR-Bd.eot")
+        format("embedded-opentype"),
+      url("../../assets/fonts/WOFF/LINESeedKR-Bd.woff") format("woff"),
+      url("../../assets/fonts/WOFF2/LINESeedKR-Bd.woff2") format("woff2");
+    font-style: normal;
+    font-weight: 700;
   }
 
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: "HallymGothic-Regular";
+    font-family: "Apple SD Gothic Neo", "LINESeedKR", Verdana, Arial, sans-serif;
   }
 
   html,
