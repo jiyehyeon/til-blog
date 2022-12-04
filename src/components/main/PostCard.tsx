34626a6ee4e737db9be2c0114947779741433dc2
Jiye <hyeonjiyeah@gmail.com>
2022-12-04 23:17:40 +0900

    feat: thumbnail 적용

--- src/components/main/PostCard.tsx
+++ src/components/main/PostCard.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent } from "react";
 import styled from "@emotion/styled";
 import { Link } from "gatsby";
+import TagList from "../blog/TagList";
 
 export type PostProps = {
   contents: any;
 };
 
 const PostCardWrapper = styled.div`
-  margin: 15px 0 45px 0;
+  padding: 20px 0;
   width: 100%;
   display: flex;
 
@@ -20,16 +21,19 @@ const ImgBox = styled.img`
   width: 40%;
   aspect-ratio: 16/9;
   object-fit: cover;
+  margin-right: 15px;
+  border-radius: 10px;
 
   @media (max-width: 600px) {
+    margin-bottom: 15px;
     width: 100%;
   }
 `;
 
 const TextBox = styled.div`
   position: relative;
-  margin-left: 10px;
-  padding: 0 10px;
+  margin-left: 15px;
+  padding: 10px 0;
 
   @media (max-width: 600px) {
     margin: 10px 0;
@@ -40,33 +44,17 @@ const Title = styled.h1`
   font-size: 30px;
   color: var(--base-dark);
   font-weight: 700;
-  line-height: 1.6;
 `;
 
 const Date = styled.div`
   @media (min-width: 600px) {
     position: absolute;
-    bottom: 0;
+    bottom: 10px;
   }
-  font-size: 14px;
-  color: var(--base-dark);
-  font-weight: 400;
+  font-size: 15px;
+  color: var(--base-grey);
+  font-weight: 500;
   margin-top: 16px;
-`;
-
-const TagList = styled.div`
-  display: flex;
-  width: 480px;
-  margin-top: 10px;
-`;
-
-const TagItem = styled.a`
-  border-radius: 25px;
-  border: 1px solid var(--base-dark);
-  padding: 5px 12px;
-  font-size: 14px;
-  margin-right: 5px;
-  color: var(--base-dark);
 `;
 
 const PostCard: FunctionComponent<PostProps> = function ({ contents }) {
@@ -78,23 +66,24 @@ const PostCard: FunctionComponent<PostProps> = function ({ contents }) {
     .split("/")
     .pop()
     .replace(".md", "");
+  console.log(contents);
 
   return (
     <PostCardWrapper>
       {/* <Link to={id}> */}
-      <ImgBox src="https://res.cloudinary.com/practicaldev/image/fetch/s--rHVl9ZcJ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/nvbddz2dlz8t3v0qcutg.PNG" />
+      <ImgBox
+        src={
+          contents.node.frontmatter.thumbnail ||
+          "https://res.cloudinary.com/practicaldev/image/fetch/s--rHVl9ZcJ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/nvbddz2dlz8t3v0qcutg.PNG"
+        }
+        alt={title}
+      />
       {/* </Link> */}
       <TextBox>
         <Link to={`/blog/${category}/${pathname}`}>
           <Title>{title}</Title>
         </Link>
-        <TagList>
-          {tags.map((tag: string) => (
-            <Link to={`/search?tag=${tag.replaceAll(" ", "")}`}>
-              <TagItem key={tag}>{tag}</TagItem>
-            </Link>
-          ))}
-        </TagList>
+        <TagList tags={tags} />
         <Date>{date}</Date>
       </TextBox>
     </PostCardWrapper>
